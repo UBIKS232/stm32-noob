978142641facf9646b78d189fca89458cf1a148d
UBIKS232 <ubik.3141@gmail.com>
2025-12-18 11:09:08 +0800

    usart finish

--- user/main.c
+++ user/main.c
@@ -5,6 +5,7 @@
 #define RCCAPB2PERIPH (RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO)
 
 void userSerialTransmit(USART_TypeDef *usartTypedef_t, uint8_t *pData, uint16_t size);
+void userSerialReceive(USART_TypeDef *usartTypedef_t, uint8_t *pData);
 int fputc(int ch, FILE *file);
 
 int main(void)
@@ -35,27 +36,32 @@ int main(void)
         .USART_BaudRate = 115200,
         // .USART_HardwareFlowControl = USART_HardwareFlowControl_None,
         .USART_Mode = USART_Mode_Tx | USART_Mode_Rx,
-        .USART_Parity = USART_Parity_Even,
-        .USART_StopBits = USART_StopBits_2,
-        .USART_WordLength = USART_WordLength_9b}; // 9b - 1b(parity) = 8b(data)
+        .USART_Parity = USART_Parity_No,
+        .USART_StopBits = USART_StopBits_1,
+        .USART_WordLength = USART_WordLength_8b}; // 9b - 1b(parity) = 8b(data)
     USART_Init(USART1, &usartInit);
     USART_Cmd(USART1, ENABLE);
 
-    uint8_t userData[2] = {0x38, 0x67};
+    uint8_t userData = 0;
 
     while (1)
     {
-        GPIO_SetBits(GPIOC, GPIO_Pin_13);
-        Delay(1000);
-        printf("Hello serial transmit.\n");
-        userSerialTransmit(USART1, userData, sizeof(userData));
-        GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-        Delay(1000);
+        userSerialReceive(USART1, &userData);
+        if (userData == '0')
+        {
+            GPIO_SetBits(GPIOC, GPIO_Pin_13); // PC13 的LED的另一端采用的是上接法
+        }
+        else
+        {
+            GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+        }
     }
 }
 
 void userSerialTransmit(USART_TypeDef *usartTypedef_t, uint8_t *pData, uint16_t size)
 {
+    if(size == 0) return;
+
     for (uint32_t i = 0; i < size; i++)
     {
         while (USART_GetFlagStatus(usartTypedef_t, USART_FLAG_TXE) == RESET)
@@ -66,6 +72,13 @@ void userSerialTransmit(USART_TypeDef *usartTypedef_t, uint8_t *pData, uint16_t 
     }
     while (USART_GetFlagStatus(usartTypedef_t, USART_FLAG_TC) == RESET)
         ;
+}
+
+void userSerialReceive(USART_TypeDef *usartTypedef_t, uint8_t *pData)
+{
+    while (USART_GetFlagStatus(usartTypedef_t, USART_FLAG_RXNE) == RESET)
+        ;
+    *pData = USART_ReceiveData(usartTypedef_t);
 }
 
 int fputc(int ch, FILE *file)
